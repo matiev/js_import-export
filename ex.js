d31a68fc9b61f94d5f9c96d3fcdabaf2d500de71
matiev <matiev@live.com>
2022-06-08 09:11:38 +0300

    fc

---
+++ ex.js
@@ -0,0 +1,8 @@
+// default по дефолту
+// let name = 'Tom';
+// export default name;
+
+// vars именнованная, через деструктуризацию
+let name = 'Mat',
+    age = 36;
+export {name, age};
---
+++ im.js
@@ -0,0 +1,8 @@
+// import name from './ex.js';
+// let ttt = document.querySelector('.ttt');
+// ttt.textContent = name;
+
+
+import {name, age} from './ex.js';
+let ttt = document.querySelector('.ttt');
+ttt.textContent = name + ' ' + age;
